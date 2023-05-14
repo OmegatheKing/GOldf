e503dd13ec1f4ef60cf3a2a08a8bb5e139b9b5e4
OmegatheKing <110073349+OmegatheKing@users.noreply.github.com>
2023-05-14 19:32:04 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { fabric } = require("./fabric");
+//const { fabric } = require("./fabric");
 
 var canvas =new fabric.Canvas('myCanvas')
 ball_y=0;
@@ -13,7 +13,7 @@ block_image_height = 5;
 function load_img(){
 	fabric.Image.fromURL("golf-h.png", function (Img) {
 	hole_obj=Img;
-	hole_obj.scaleToWitdh(50);
+	hole_obj.scaleToWidth(50);
 	hole_obj.scaleToHeight(50);
 	hole_obj.set({
 	top:hole_y,
@@ -26,9 +26,9 @@ function load_img(){
 
 function new_image()
 {
-	fabric.Image.fromURL("golf-h.png", function (Img) {
+	fabric.Image.fromURL("ball.png", function (Img) {
 		ball_obj=Img;
-		ball_obj.scaleToWitdh(50);
+		ball_obj.scaleToWidth(50);
 		ball_obj.scaleToHeight(50);
 		ball_obj.set({
 		top:ball_y,
